91b7b944c86ed82a939d9afb65e987cdd5b288a8
Nandhu Sathish <writetonandhusathish@gmail.com>
2025-11-30 17:18:30 +0530

    env configured

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
+import { Config } from './config/index.js';
+
 function welcome(name: string) {
-  console.log("Welcome" + name);
+    console.log('Welcome' + name);
 }
 
-welcome("nandhu");
+welcome('nandhu');
+
+console.log(Config.PORT);
