7ba5fd50d34e566dd8bc3dd3d9cce866e4fe82d4
Nandhu Sathish <writetonandhusathish@gmail.com>
2025-12-02 21:54:34 +0530

    eslint config update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,6 +15,11 @@ export default defineConfig(
         },
     },
     {
-        ignores: ['dist', 'node_modules', 'eslint.config.mjs'],
+        ignores: [
+            'dist',
+            'node_modules',
+            'eslint.config.mjs',
+            'jest.config.js',
+        ],
     },
 );
